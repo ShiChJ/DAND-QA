af43ecdc9c13c754fccdf3659dbe7701836f41c8
Jerry Shi <shichengjie1996@gmail.com>
2018-01-21 15:36:23 +0900

    Update-20180121

--- Intro-Python/Python入门习题答案.py
+++ Intro-Python/Python入门习题答案.py
@@ -189,6 +189,28 @@ for headline in headlines:
 
 print(len(news_ticker))
 
+#重构代码
+def check_answer(my_answer, answer):
+    if my_answer == answer:
+        return True
+    else:
+        return False
+
+def check_answers(my_answers,answers):
+    #Checks the five answers provided to a multiple choice quiz and returns the results.
+    results = []
+    for i in range(len(my_answers)):
+        results.append(check_answer(my_answers[i], answers[i]))
+    count_correct = 0
+
+    for result in results:
+        if result:
+            count_correct += 1
+
+    if count_correct/len(results) > 0.7:
+        return "Congratulations, you passed the test!"
+    elif (len(results) - count_correct)/len(results) >= 0.3:
+        return "Unfortunately, you did not pass."
 
 #用户（按国家/地区）
 from countries import country_list # Note: since the list is so large, it's tidier

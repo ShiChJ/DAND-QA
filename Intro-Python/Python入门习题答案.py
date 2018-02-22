a6071e9905e3f6993ef52cb9e9170d01431e43ad
Jerry Shi <shichengjie1996@gmail.com>
2018-02-22 17:26:59 +0800

    Update-20180222

--- Intro-Python/Python入门习题答案.py
+++ Intro-Python/Python入门习题答案.py
@@ -313,6 +313,29 @@ def print_list(l, numbered = False, bullet_character = '-'):
 import math
 print(math.exp(3))
 
+#密码生成器
+# Use an import statement at the top
+import random
+word_file = "words.txt"
+word_list = []
+
+#fill up the word_list
+with open(word_file,'r') as words:
+	for line in words:
+		# remove white space and make everything lowercase
+		word = line.strip().lower()
+		# don't include words that are too long or too short
+		if 3 < len(word) < 8:
+			word_list.append(word)
+
+# Add your function generate_password here
+# It should return a string consisting of three random words
+# concatenated together without spaces
+def generate_password():
+    return str().join(random.sample(word_list,3))
+
+# test your function
+print(generate_password())
 
 #将链接添加到文章链
 def web_crawl():
